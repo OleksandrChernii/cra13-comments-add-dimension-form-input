ce6e0cf7dbacb8163ba1fd37af00cc804e61975a
AlexCherny962 <53741736+AlexCherny962@users.noreply.github.com>
2020-08-01 08:25:29 +0300

    Updates

---
+++ precache-manifest.9b8cd5fa8a9893c7db776369491a6833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bda9a914ffb83ac60acfede503153c6",
+    "url": "/cra13-comments-add-dimension-form-input/index.html"
+  },
+  {
+    "revision": "95777a7a7ccc864bcd59",
+    "url": "/cra13-comments-add-dimension-form-input/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1b59851e4ac061cb879",
+    "url": "/cra13-comments-add-dimension-form-input/static/js/2.dc2a1651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra13-comments-add-dimension-form-input/static/js/2.dc2a1651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95777a7a7ccc864bcd59",
+    "url": "/cra13-comments-add-dimension-form-input/static/js/main.46e42659.chunk.js"
+  },
+  {
+    "revision": "833c59a1cc6309cedf87",
+    "url": "/cra13-comments-add-dimension-form-input/static/js/runtime-main.9c4bab9d.js"
+  }
+]);
